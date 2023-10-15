5b050629b702c4aba1748cda4ee3f554649e0806
TheXro <ravyyadav97@gmail.com>
2023-10-15 17:22:29 +0530

    looks good

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <div className='flex flex-col justify-center items-center h-screen bg-gray-100 dark:bg-gray-800 dark:text-white text-gray-900 w-[100vw]'>
-        <div className='flex items-center justify-center m-8'>
+      <div className='flex flex-col justify-center items-center h-screen dark:bg-gray-900 dark:text-white text-gray-900 w-[100vw]'>
+        <div className='flex items-center justify-center m-10'>
           <img
             src='./logo.svg'
             alt='logo'
             className='p-2'
-            width={50}
-            height={50}
+            width={60}
+            height={60}
           />
           <span className='text-4xl font-[600] tracking-wide '>devlinks</span>
         </div>
@@ -25,19 +25,42 @@ const Login = () => {
             <form>
               <div className='text-left'>
                 <div className='mt-8 mb-2 text-[14px]'>Email address</div>
-                <div className=''>
-                  <div className='flex gap-3 p-3 ease-in-out relative'>
-                    <img src='./images/icon-email.svg' alt='' className="absolute " />
-                    <input
-                      type='email'
-                      className="pl-8 pr-16 py-2 border outline-none w-[80%] bg-transparent   focus:border-indigo-600   "
-                      required
-                      placeholder='e.g. thexro@gmail.com'
-                    />
-                  </div>
+
+                <div className='inputForm flex gap-3 border-[1.5px] border-slate-500 rounded-md p-3 transition duration-150 ease-in relative'>
+                  <img src='./images/icon-email.svg' alt='' />
+                  <input
+                    type='email'
+                    className='input border-none focus:outline-0 w-[80%] bg-transparent placeholder:opacity-70  '
+                    required
+                    placeholder='notGoodEmail@gmail.com'
+                  />
                 </div>
               </div>
+              <div className='text-left'>
+                <div className='mt-6 mb-2 text-[14px]'>Password</div>
+
+                <div className='inputForm flex gap-3 border-[1.5px] border-slate-500 rounded-md p-3 transition duration-150 ease-in relative'>
+                  <img src='./images/icon-password.svg' alt='' />
+                  <input
+                    type='password'
+                    className='input border-none focus:outline-0 w-[80%] bg-transparent placeholder:opacity-70  '
+                    required
+                    placeholder='strong password'
+                  />
+                </div>
+              </div>
+              <div className='mt-8'>
+                <button className='px-4 py-2 w-[100%] text-white border-[1.5px] border-slate-600 rounded-lg duration-200 hover:border-gray-900 hover:bg-slate-800 active:text-gray-500 active:shadow-lg'>
+                  <span className='font-[600]'>Login</span>
+                </button>
+              </div>
             </form>
+            <div className="mt-10">
+              <span className=' text-slate-400 mr-1 '>
+                Don't have an account?
+              </span>
+              <a href="#">Create Account</a>
+            </div>
           </div>
         </div>
       </div>
